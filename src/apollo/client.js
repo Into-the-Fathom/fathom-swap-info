7983b6b52991fda5101ac3c210f5f58de55770a4
ztshort <ztshort@hotmail.com>
2023-04-24 14:29:25 -0500

    fix subgraph url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -12,7 +12,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://graph.fathom-labs.app/graphql',
+    uri: 'https://dev.fathom-labs.app/graphql',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
