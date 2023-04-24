e0461911dd6cf7b8428249ced1f14c568051d6c3
ztshort <ztshort@hotmail.com>
2023-04-24 15:27:35 -0500

    update address and subgraph urls

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://graph.composer.live/subgraphs/name/dex-subgraph',
+    uri: 'https://graph.fathom-labs.app/subgraphs/name/dex-subgraph',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -12,7 +12,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://dev.fathom-labs.app/graphql',
+    uri: 'https://graph.fathom-labs.app/graphql',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
