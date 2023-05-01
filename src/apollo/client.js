cef6c5c9e6a78f5e2b0a8ec8f4ebb5f32903e80c
ztshort <ztshort@hotmail.com>
2023-04-30 19:49:52 -0500

    update graph url to mainnet graph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:8000/subgraphs/name/dex-subgraph',
+    uri: 'http://xinfin-graph.fathom-labs.app/subgraphs/name/dex-subgraph',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
