9b302127b63034bc5892ee54bdec2897937c517a
ztshort <ztshort@hotmail.com>
2023-06-02 12:40:14 -0500

    update fxd-usdt address

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const FACTORY_ADDRESS = '0xa7bf3E5eDbC8d37584218778cd6A1EDe91Bf201D'
+export const FACTORY_ADDRESS = '0x9fAb572F75008A42c6aF80b36Ab20C76a38ABc4B'
 
 export const BUNDLE_ID = '1'
 
@@ -13,31 +13,24 @@ export const timeframeOptions = {
 
 // token list urls to fetch tokens from - use for warnings on tokens and pairs
 export const SUPPORTED_LIST_URLS__NO_ENS = [
-  'https://raw.githubusercontent.com/Into-the-Fathom/fathom-swap-default-token-list/dev/src/tokenlists/apothem.json',
+  'https://raw.githubusercontent.com/Into-the-Fathom/fathom-swap-default-token-list/master/src/tokenlists/xdc.json',
 ]
 
 // hide from overview list
 export const TOKEN_BLACKLIST = [
   // rebass tokens
-  '0x82b4334f5cd8385f55969bae0a863a0c6ea9f63f', // old US+
-  '0xcbb5e38427f102fdb08d76bb592e85d454a31d05', // old FXD
-  '0xfbf03b959b9836ac1cc13a8b30f228056dba7b9a', // old FTHM
+  '0x0000000000000000000000000000000000000000',
 ]
 
 // pair blacklist
 export const PAIR_BLACKLIST = [
-  '0x56ef3ac9b4ba32683def994d640f3753ad1f808d', // old XDC/US+
-  '0x73a8af48ec87ffb75b8e0deec5b8bf42fae00bee', // old XDC/FXD
-  '0x0ed1488913d90ffd16b4a10a2191fecf621909f0', // old USDTx/FXD
-  '0x63da6b37ac3893390c0dc4d227d7d42e98e4205b', // old US+/FXD
-  '0xa73a803562c80727a76514fb0a51427cc9917cd7', // old FTHM/FXD
-  '0xe52c709606d0ad48f367a066b95c2c95a197e8a9', // old FTHM/FXD
-  '0x91fda7c38b2dfda74868b40d1cf478d3649a2743', // old FTHM/WXDC
+  '0x7ce2cacc5ad4fe57453bdf0073e1a1c3e587c7c1', // WXDC-TEST
+  '0x17da34c87c20846bd66baad60cccfb438989b744', // TEST-WXDC
 ]
 
 // warnings to display if page contains info about blocked token
 export const BLOCKED_WARNINGS = {
-  '0xf4eda77f0b455a12f3eb44f8653835f377e36b76':
+  '0x0000000000000000000000000000000000000000':
     'TikTok Inc. has asserted this token is violating its trademarks and therefore is not available.',
 }
 
@@ -55,5 +48,5 @@ export const TRACKED_OVERRIDES_PAIRS = ['0x0000000000000000000000000000000000000
 // all pairs that include token will be tracked
 export const TRACKED_OVERRIDES_TOKENS = ['0x0000000000000000000000000000000000000000']
 
-export const FXD_US_PLUS_PAIR_ID = '0x119dfbe4dd4ed107a42da4a76897c3f7df816de4'
-export const FTHM_US_PLUS_PAIR_ID = '0x119dfbe4dd4ed107a42da4a76897c3f7df816de4' // TODO: update after FTHM/USDT pair is created
+export const FXD_US_PLUS_PAIR_ID = '0x4821cdc4b01bfee77c6ab8abc9348c4351600e26'
+export const FTHM_US_PLUS_PAIR_ID = '0x4821cdc4b01bfee77c6ab8abc9348c4351600e26' // TODO: update after FTHM-USD+ pair is created
